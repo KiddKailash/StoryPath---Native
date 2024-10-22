0626c7d78314b102f67787c590fcdf7b5af73ab6
Kailash Kidd <kailashkidd@Kailashs-MacBook-Pro.local>
2024-10-23 09:09:21 +1000

    Initial commit
    Generated by create-expo-app 3.0.0.

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,6 @@
-// babel.config.js
-
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      ['module:react-native-dotenv', {
-        moduleName: '@env',
-        path: '.env',
-      }],
-    ],
   };
 };
